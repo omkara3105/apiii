84d97425678840199ecd6e5cf5a19c418a4932d2
om <dhekaleomkar1@gmail.com>
2022-10-19 00:45:13 +0530

    omi

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ const express = require("express");
 const cros = require("cros");
 const app=express();
 
+app.use(cors());
+
 const port =process.env.PORT || 3000;
 
+const apiData=require("./data.json");
+
+app.get("/",(req,res)=>{
+    res.send("hemllo")
+});
+
+app.get("/service",(req,res)=>{
+    res.send(apidata)
+})
+
+app.listen(port,()=>{
+    console.log("i live")
+})
